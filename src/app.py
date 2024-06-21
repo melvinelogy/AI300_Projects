41e97e355b94b59b5acaa24441e76003d89dcee6
Kathy Ong Chu Quan <kathyong93@gmail.com>
2024-06-22 01:19:14 +0800

    modified:   .DS_Store 	modified:   .ipynb_checkpoints/EDA-checkpoint.ipynb 	modified:   EDA.ipynb     modified:   catboost_info/catboost_training.json 	modified:   catboost_info/learn/events.out.tfevents 	modified:   catboost_info/time_left.tsv 	renamed:    requirement.txt -> requirements.txt 	new file:   src/__pycache__/model.cpython-311.pyc 	modified:   src/app.py 	modified:   src/model.py 	modified:   src/templates/index.html

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from model import Model
 
 app = Flask(__name__)
 
@@ -9,6 +10,48 @@ def home():
     if request.method == 'POST':
         form_input = dict(request.form)
         print(form_input)
+
+        # Modified code
+        account_id = form_input['account_id']
+        has_internet_service = form_input['has_internet_service']
+        internet_type = form_input['internet_type']
+        has_unlimited_data = form_input['has_unlimited_data']
+        has_multiple_lines = form_input['has_multiple_lines']
+        contract_type = form_input['contract_type']
+        paperless_billing = form_input['paperless_billing']
+        payment_method = form_input['payment_method']
+        city = form_input['city']
+        stream_tv = form_input['stream_tv']
+        stream_movie = form_input['stream_movie']
+        gender = form_input['gender']
+        senior_citizen = form_input['senior_citizen']
+
+        model_inputs = [ account_id,
+    has_internet_service,
+    internet_type,
+    has_unlimited_data,
+    has_multiple_lines,
+    contract_type,
+    paperless_billing,
+    payment_method,
+    stream_tv,
+    stream_movie,
+    gender,
+    senior_citizen,
+    city]
+        prediction = Model().predict(model_inputs)
+
+        if prediction  == 1:
+            prediction_label = 'Joined'
+        elif prediction  == 2:
+            prediction_label = 'Stayed'
+        elif prediction  == 3:
+            prediction_label = 'Churned'
+        else:
+            prediction_label = 'unknown'
+
+        print(Model().predict(model_inputs)) 
+        return render_template('index.html', prediction=prediction_label)
 
     return render_template('index.html')
 
--- src/model.py
+++ src/model.py
@@ -6,3 +6,26 @@ class Model:
 
     def predict(self, input_features):
         return self.model.predict(input_features)
+
+        # Add in this code chunk temporarily (delete it after this run)
+
+
+cat_features = {
+    "account_id": "AFEO-XOOCP",
+    "has_internet_service": 1,
+    "internet_type": "Fiber Optic",
+    "has_unlimited_data": 1,
+    "has_multiple_lines": 1,
+    "contract_type": "Two Year",
+    "paperless_billing": 1,
+    "payment_method": "Credit Card",
+    "stream_tv": 1,
+    "stream_movie": 1,
+    "gender": "Female",
+    "senior_citizen": 1,
+    "city": "Camarillo"
+}
+model_inputs = list(cat_features.values())
+
+print(model_inputs)                  
+print(Model().predict(model_inputs)) 
